edd1fb2f6ae1c1c5babaf93f54c73d81a209d451
name_snrl <Demogorgon-74@ya.ru>
2024-01-19 22:50:25 +0500

    add pkgs to shell

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
-      with nixpkgs.legacyPackages.${system};
+      with import nixpkgs { inherit system; config.allowUnfree = true; };
       {
         devShells.default =
           let
@@ -25,6 +25,20 @@
               sbt
               jdk17
               scalafmt
+              neovim-unwrapped
+              metals
+              #(vscode-with-extensions.override {
+              #  vscodeExtensions = [
+              #    (vscode-utils.buildVscodeMarketplaceExtension {
+              #      mktplcRef = {
+              #        name = "metals";
+              #        publisher = "scalameta";
+              #        version = "1.27.0";
+              #        sha256 = "sha256-ilqr0Z56553FtBG/e0cmu1uwUbWFSGe2mxfkTt6uyZs=";
+              #      };
+              #    })
+              #  ];
+              #})
             ];
           };
       });
